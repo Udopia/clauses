cf4ca0a1bc8ab22b962b2c1c401d446e651af93e
Markus Iser <markus.iser@kit.edu>
2016-03-10 12:43:54 +0100

    buildfix

---
+++ rosi.cc
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  int zahl = 0;
+  while (zahl < 10) {
+    cout << "Hallo" << endl;
+    zahl = zahl + 1;
+    cout << zahl << endl;
+  }
+}
+
